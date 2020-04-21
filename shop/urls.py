8eb5142907c1fae0a5e6f5912d2affb714e1d9d1
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-21 14:39:31 +0545

    create url view model and super user

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,11 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.index, name='ShopHome')
+    path('', views.index, name='ShopHome'),
+    path('about/', views.about, name='AboutUs'),
+    path('contact/', views.contact, name='ContactUs'),
+    path('tracker/', views.tracker, name='TrackingStatus'),
+    path('search/', views.search, name='Search'),
+    path('productview/', views.prodview, name='ProductView'),
+    path('checkout/', views.checkout, name='Checkout'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -5,3 +5,27 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+
+def about(request):
+    return HttpResponse("We are at about us view page")
+
+
+def contact(request):
+    return HttpResponse("We are at contact view page")
+
+
+def tracker(request):
+    return HttpResponse("We are at tracker view page")
+
+
+def search(request):
+    return HttpResponse(" We are at search view page")
+
+
+def prodview(request):
+    return HttpResponse("We are at roduct view page")
+
+
+def checkout(request):
+    return HttpResponse("We are at checkout view page")
