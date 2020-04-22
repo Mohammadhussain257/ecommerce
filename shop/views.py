42c9638eda9acae427c032ee845288cb5a3d90c6
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-22 15:55:26 +0545

    display dynamic product category wise

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    all_products = []
+    product_category = Product.objects.values('category', 'id')
+    category = {item['category'] for item in product_category}
+    for cat in category:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        no_of_slides = n // 4 + ceil((n / 4) - (n // 4))
+        all_products.append([prod, range(1, no_of_slides), no_of_slides])
+    context = {'all_products': all_products}
+    return render(request, 'shop/index.html', context)
 
 
 def about(request):
